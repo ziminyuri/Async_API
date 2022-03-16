f7cd84258d8bfc263fb1c07cdcb8fcaa63f4dd70
dmitryvokhmin <boxdima1@gmail.com>
2022-03-16 18:51:00 -0400

    добавил файлы для моделей

--- src/main.py
+++ src/main.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
-        port=8000,
+        port=80,
         log_config=LOGGING,
         log_level=logging.DEBUG,
     )
--- src/models/film.py
+++ src/models/film.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 import orjson
 from pydantic import BaseModel
@@ -8,8 +8,15 @@ from helpers import orjson_dumps
 
 class Film(BaseModel):
     id: str
+    imdb_rating: Optional[str]
+    genre: List[str] = []
     title: str
     description: Optional[str]
+    director: List[str] = []
+    actors_names: List[str] = []
+    writers_names: List[str] = []
+    actors: List[dict] = []
+    writers: List[dict] = []
 
     class Config:
         json_loads = orjson.loads

ea4062d74a9315641aaba15f37e4e4f906ae97df
ziminyuri <zimin.yuri22@gmail.com>
2022-03-28 21:11:09 +0700

    Вынес текстовые сообщения для пользователей вынес в dict

--- src/api/v1/genre.py
+++ src/api/v1/genre.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from api.v1.query_params import BaseParams
 from models.genre import Genre, Genres
 from services.genre import GenreService, get_genre_service
+from dictionary import errors_dict
 
 router = APIRouter()
 
@@ -21,7 +22,7 @@ async def genres_list(
 
     genre_list = await genres_service.get_by_params(params)
     if not genre_list:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genre not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=errors_dict['404_genres'])
     return Genres.parse_obj(genre_list.__root__)
 
 
@@ -33,6 +34,6 @@ async def genre_details(
 
     genre = await genre_service.get_by_id(genre_id)
     if not genre:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genre not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=errors_dict['404_genre'])
 
     return Genre(**genre.dict())
---
+++ src/dictionary.py
@@ -0,0 +1,8 @@
+errors_dict = {
+    '404_film': 'film not found',
+    '404_films': 'films not found',
+    '404_genre': 'genre not found',
+    '404_genres': 'genres not found',
+    '404_person': 'person not found',
+    '404_persons': 'persons not found'
+}
